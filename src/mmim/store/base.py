85ae2a4b54e5b3ef95475dd4ebbd06c2c34eb158
Antonio Galanti <antonio.galanti@gmail.com>
2026-05-08 12:08:43 +0200

    store: interface now has defaults for read_*

--- src/mmim/store/base.py
+++ src/mmim/store/base.py
@@ -11,12 +11,18 @@ def gen_path(base: Path, prefix: str | None, suffix: str) -> Path:
 
 
 class ReadOnlyStore(ABC):
+    # TODO: support passing a lambda to transform from the reader
+    # + returning ContextManager[TextIO] and ContextManager[BinaryIO] in open_text() and open_binary()
+    # @staticmethod
+    # open_text_and_then(self, path: str, op: Callable[[TextIO], T], with_prefix: bool = True)
+    # @staticmethod
+    # open_binary_and_then(self, path: str, op: Callable[[BinaryIO], T], with_prefix: bool = True)
     @abstractmethod
-    def read_text(self, path: str, with_prefix: bool) -> str:
+    def read_text(self, path: str, with_prefix: bool = True) -> str:
         raise NotImplementedError
 
     @abstractmethod
-    def read_bytes(self, path: str, with_prefix: bool) -> bytes:
+    def read_bytes(self, path: str, with_prefix: bool = True) -> bytes:
         raise NotImplementedError
 
     @abstractmethod
